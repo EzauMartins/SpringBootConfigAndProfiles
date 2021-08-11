f99fda3c1d201bde0a3ad803c81053a152ad07df
Ezau Martins <ezaumateus97@gmail.com>
2021-08-10 22:37:37 -0400

    TesteMapearVariavelAbiente

--- src/main/java/com/digitalInnovation/springBoot/appController.java
+++ src/main/java/com/digitalInnovation/springBoot/appController.java
@@ -11,8 +11,19 @@ public class appController {
     @Value("${app.message}")
     private String appMessage;
 
+    //VARIAVEL DE AMBIENTE  -- MAPEADA
+    @Value("${ENV_DB_UR:NENHUMA}")  //Caso n seja informada retorna Nenhuma
+    private String dbEnvironmentVariable;
+
+
     @GetMapping("/")
     public String getAppMessage(){
         return appMessage;
+    }
+
+
+    @GetMapping("/envVariable")
+    public String getDbEnvironmentVariable(){
+        return "Variavel de ambiente passada: " + dbEnvironmentVariable;
     }
 }
